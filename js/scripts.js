f453ada34cbe02c523181ed09cc9e71f9d4dbcaa
VadicRt <vadicrugun@gmail.com>
2021-10-11 13:24:52 +0300

    changed on the fourth day

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,20 @@
-const schemeSvg = document.querySelector('.scheme-svg'),
-	  totalPriceTag = document.querySelector('.price-total');
+const schemeSvg 	= document.querySelector('.scheme-svg'),
+	  totalPriceTag = document.querySelector('.price-total'),
+	  menuButton 	= document.querySelector('.m-menu'),
+	  menu 			= document.querySelector('.menu');
 
-let cost = 800,
-	totalPrice = 0;
+let cost 		= 800,
+	totalPrice  = 0;
 
-schemeSvg.addEventListener('click', (e) => {
+schemeSvg.addEventListener('click', e => {
 	if (!e.target.classList.contains('booked') && !e.target.classList.contains('light')) {
 		e.target.classList.toggle('active');
 		let totalSeats = schemeSvg.querySelectorAll('.active').length;
 		totalPrice = totalSeats * cost;
 		totalPriceTag.textContent = totalPrice; 
 	}
+})
+
+menuButton.addEventListener('click', () => {
+	menu.classList.toggle('is-open');
 })
